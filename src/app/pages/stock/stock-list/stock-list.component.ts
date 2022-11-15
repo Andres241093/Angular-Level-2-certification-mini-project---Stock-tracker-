46475bc4305fa0af351f35cba5a8df1b3e8f55b5
Andres241093 <72680557+Andres241093@users.noreply.github.com>
2022-11-15 19:41:20 -0400

    7 files modified

--- src/app/pages/stock/stock-list/stock-list.component.ts
+++ src/app/pages/stock/stock-list/stock-list.component.ts
@@ -1,15 +1,24 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-stock-list',
   templateUrl: './stock-list.component.html',
-  styleUrls: ['./stock-list.component.css']
+  styleUrls: ['./stock-list.component.css'],
 })
 export class StockListComponent implements OnInit {
+  searchControl: FormControl = new FormControl('');
+  searchValue: string = '';
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
+  ngOnInit(): void {
+    this.searchControl.valueChanges.subscribe({
+      next: (value) => (this.searchValue = value),
+    });
   }
 
+  search(): void {
+    console.log(this.searchValue);
+  }
 }
--- src/app/pages/stock/stock.module.ts
+++ src/app/pages/stock/stock.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { StockListComponent } from './stock-list/stock-list.component';
 import { StockRoutingModule } from './stock-routing.module';
-import { CardModule } from 'src/app/shared/card/card.module';
+import { CardModule } from '../../shared/card/card.module';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
-  imports: [CommonModule, StockRoutingModule, CardModule],
+  imports: [CommonModule, StockRoutingModule, CardModule, ReactiveFormsModule],
   declarations: [StockListComponent],
 })
 export class StockModule {}
