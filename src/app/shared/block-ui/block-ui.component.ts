5b132fb4c529c887bd78a7c49c3e0f555cd41567
Andres241093 <72680557+Andres241093@users.noreply.github.com>
2022-11-16 13:50:49 -0400

    1 file modified, 1 added, 1 deleted

--- src/app/shared/block-ui/block-ui.component.ts
+++ src/app/shared/block-ui/block-ui.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { BlockUiService } from '../../services/block-ui.service';
 
 @Component({
